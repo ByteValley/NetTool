cec1774ea852bea3aa7a8bc6c607b4c6e4d1c5f2
Sean <118089756+SeannLiang@users.noreply.github.com>
2024-09-02 15:54:49 +0800

    Update sub_info_panel.js

--- Script/sub_info_panel.js
+++ Script/sub_info_panel.js
@@ -15,7 +15,8 @@
   let used = info.download + info.upload;
   let total = info.total;
   let expire = args.expire || info.expire;
-  let content = [`已用：${toPercent(used, total)} \t|  用量：${bytesToSize(used)} \n剩余：${toReversePercent(used, total)} \t|  余量：${toMultiply(total, used)}`];
+  // let content = [`已用：${toPercent(used, total)} \t|  用量：${bytesToSize(used)} \n剩余：${toReversePercent(used, total)} \t|  余量：${toMultiply(total, used)}`];
+  let content = [`<div  style="width: 10vw ;color: red;" class='child'>已用：${toPercent(used, total)}</div> |`  用量：${bytesToSize(used)} \n剩余：${toReversePercent(used, total)} \t|  余量：${toMultiply(total, used)}`];
 
   if (resetDayLeft || expire) {
     if (resetDayLeft && expire && expire !== "false") {
