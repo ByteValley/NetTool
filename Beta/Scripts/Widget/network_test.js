34f7abd8d3ef26deede7adf593d75969f6526f3e
ByteValley <118089756+ByteValley@users.noreply.github.com>
2026-03-19 19:27:35 +0800

    Update network_test.js

--- Beta/Scripts/Widget/network_test.js
+++ Beta/Scripts/Widget/network_test.js
@@ -95,7 +95,7 @@ export default async function (ctx) {
     type: "widget",
     padding: [16, 16, 16, 16],
     backgroundColor: "transparent",
-    refreshAfter: new Date(0).toISOString(),
+    refreshAfter: new Date(Date.now() + 60 * 1000).toISOString(),
     children: [
       // 第一层：标题与药丸描述
       {
